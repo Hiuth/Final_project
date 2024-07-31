7ac5b29b7a55c6806de50f63e5a8890287d4d5bc
Hiuth <Hiuth2004@gmail.com>
2024-07-31 18:26:26 +0700

    update

--- customer/js/main.js
+++ customer/js/main.js
@@ -1,10 +1,10 @@
-// nút tăng giảm số lượng mặt hàng trong giỏ hàng
-
 // đưa mặt hàng vào giỏ hàng
 const addIntoCart = document.querySelectorAll(".add-cart-button");
 //console.log(addIntoCart);
 
 var productList = JSON.parse(localStorage.getItem("productList")) || [];
+
+var productList = getProductList();
 
 //thêm sản phẩm vào giỏ hàng
 addIntoCart.forEach(function (button, index) {
@@ -16,10 +16,16 @@ addIntoCart.forEach(function (button, index) {
       var product = btnItem.closest(".product-img"); //chọn thẻ cha đang chưa cái nút thêm vào
       var productImg = product.querySelector("img").src;
       var productName = product.querySelector(".name").children[0].innerText;
+      var productQuantity = 1;
       var productPrice = product.querySelector(".price").innerText;
       //console.log(productImg, productName, productPrice);
       //lưu giỏ hàng lên sessionStorage
-      var list = new Array(productImg, productName, productPrice);
+      var list = new Array(
+        productImg,
+        productName,
+        productQuantity,
+        productPrice
+      );
       productList.push(list);
       localStorage.setItem("productList", JSON.stringify(productList));
     } //stringify chuyển hết tất cả kiểu hết về string
@@ -27,21 +33,15 @@ addIntoCart.forEach(function (button, index) {
   });
 });
 
-function changeUnit(x) {
-  var Unit_change = x.toLocaleString("vi-VN", {
-    style: "currency",
-    currency: "VND",
-    minimumFractionDigits: 0,
-    maximumFractionDigits: 0,
-  });
-  return Unit_change;
-}
-
 function ShowCart() {
   var addCart = localStorage.getItem("productList");
   var cartList = JSON.parse(addCart); //chuyển từ string về các kiểu dữ liệu mặc định
   var myCart = "";
+  var total = 0;
   for (var i = 0; i < cartList.length; i++) {
+    var unitPrice = parseInt(cartList[i][3].replace(/\./g, ""), 10);
+    var quantity = parseInt(cartList[i][2], 10);
+    total = unitPrice * quantity;
     myCart +=
       '<tr> <td class="product-in-cart"> <button class="delete">X</button> <a href=""> <img class="img-cart" src="' +
       cartList[i][0] +
@@ -51,12 +51,14 @@ function ShowCart() {
       cartList[i][2] +
       '</p> <p class="unit-price">VND</p> </div></td>' +
       '<td class="quantity-button">' +
-      '<div class="but"> <button>-</button>' +
-      '<input type="text" name="" id="quantity" value="1"> <button>+</button> </div> </td>' +
+      '<div class="but"> <button class="minus-btn" onclick="handleMinus(this)">-</button>' +
+      '<input type="text" name="quantity" id="quantity" value="' +
+      cartList[i][3] +
+      '"> <button class="plus-btn" onclick="handlePlus(this)">+</button> </div> </td>' +
       '<td class="product-subtotal">' +
       '<div class="price-wallpaper">' +
       '<p class="price">' +
-      cartList[i][2] +
+      total.toLocaleString("de-DE") +
       '</p><p class="unit-price">VND </p> </div> </td> </tr>';
   }
   document.getElementById("List").innerHTML = myCart;
