cd4a6e87f2060dfb0016572220f36046d3e7f4b8
Hiuth <Hiuth2004@gmail.com>
2024-07-31 15:23:44 +0700

    update

--- customer/main.js
+++ customer/main.js
@@ -4,13 +4,14 @@
 const addIntoCart = document.querySelectorAll(".add-cart-button");
 //console.log(addIntoCart);
 
-var productList = JSON.parse(sessionStorage.getItem("productList")) || [];
+var productList = JSON.parse(localStorage.getItem("productList")) || [];
 
 //thêm sản phẩm vào giỏ hàng
 addIntoCart.forEach(function (button, index) {
   //tạo ra sự kiện nhấn vào nút thêm giỏ hàng
   button.addEventListener("click", function (event) {
     {
+      event.preventDefault();
       var btnItem = event.target; //xác định đúng phần tử đang click vào
       var product = btnItem.closest(".product-img"); //chọn thẻ cha đang chưa cái nút thêm vào
       var productImg = product.querySelector("img").src;
@@ -20,13 +21,24 @@ addIntoCart.forEach(function (button, index) {
       //lưu giỏ hàng lên sessionStorage
       var list = new Array(productImg, productName, productPrice);
       productList.push(list);
-      sessionStorage.setItem("productList", JSON.stringify(productList));
+      localStorage.setItem("productList", JSON.stringify(productList));
     } //stringify chuyển hết tất cả kiểu hết về string
+    window.location.assign("Gio_Hang_Co_Hang.html");
   });
 });
 
+function changeUnit(x) {
+  var Unit_change = x.toLocaleString("vi-VN", {
+    style: "currency",
+    currency: "VND",
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 0,
+  });
+  return Unit_change;
+}
+
 function ShowCart() {
-  var addCart = sessionStorage.getItem("productList");
+  var addCart = localStorage.getItem("productList");
   var cartList = JSON.parse(addCart); //chuyển từ string về các kiểu dữ liệu mặc định
   var myCart = "";
   for (var i = 0; i < cartList.length; i++) {
@@ -51,9 +63,21 @@ function ShowCart() {
 }
 
 function ShowTotal() {
-  var addCart = sessionStorage.getItem("productList");
+  var addCart = localStorage.getItem("productList");
   var cartList = JSON.parse(addCart);
-  var total = 0;
-  for (let i = 0; i < cartList.length; i++) {}
-  total += cartList[i][2];
+  var pro_total = 0;
+  var cartTotal = "";
+  for (var i = 0; i < cartList.length; i++) {
+    pro_total += parseInt(cartList[i][2].replace(/\./g, ""), 10);
+  }
+  var Unit_change = pro_total.toLocaleString("de-DE");
+  cartTotal +=
+    '<tr><th>Tạm tính</th><td class="product-price"><div class="price-wallpaper"> <p class="price">' +
+    Unit_change +
+    '</p><p class="unit-price">VND</p></div>' +
+    '</td> </tr><tr><th>Giao hàng</th><td class="delivery"> <p>  Chi phí được tính theo phí của Giao Hàng Tiết Kiệm. Quý khách vui lòng thanh toán khi nhận hàng</p>' +
+    '</td></tr><tr> <th>Tổng</th> <td class="product-price"> <div class="price-wallpaper"><p class="price">' +
+    Unit_change +
+    '</p> <p class="unit-price">VND</p> </div></td></tr>';
+  document.getElementById("total").innerHTML = cartTotal;
 }
